80afd82c130953a254bc4972f769ff2a6b6ecef2
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-08-16 02:58:44 -0700

    mule

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,30 +1,38 @@
 #!/usr/bin/env python3
 '''A module with tools for request caching and tracking.
 '''
-import requests
 import redis
-import functools
+import requests
+from functools import wraps
+from typing import Callable
 
-CACHE_EXPIRATION = 10
 
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
+
+
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
+    @wraps(method)
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
+
+
+@data_cacher
 def get_page(url: str) -> str:
-    cache = redis.Redis()
-    count_key = f"count:{url}"
-    content_key = f"content:{url}"
-
-    # Check if the content is already cached
-    cached_content = cache.get(content_key)
-    if cached_content is not None:
-        return cached_content.decode("utf-8")
-
-    # Fetch the page content
-    response = requests.get(url)
-    content = response.text
-
-    # Cache the content with an expiration time
-    cache.setex(content_key, CACHE_EXPIRATION, content)
-
-    # Track the count of URL accesses
-    cache.incr(count_key)
-
-    return content
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
+    return requests.get(url).text
