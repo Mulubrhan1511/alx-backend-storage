3b7530ca060187b3cd10065c55ed9bc53d2f0381
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-08-18 00:29:27 -0700

    mule

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,7 +6,8 @@ import redis
 import requests
 from typing import Callable
 
-redis_ = redis.Redis
+redis_ = redis.Redis()
+
 
 def count_requests(method: Callable) -> Callable:
     """ Decortator for counting """
